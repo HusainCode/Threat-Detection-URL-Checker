bf5ff77b97f6afbebaeeb5c6b2d2dd12b1318e93
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-03-02 15:19:22 -0600

    Create logger.py

---
+++ src/logger.py
@@ -0,0 +1,16 @@
+# Logger is responsible
+#
+#  Purpose:
+#
+#  Key Attributes:
+#
+#
+#  Main Methods:
+#
+# Example:
+# [2024-03-02 14:30:10] ERROR: Failed to fetch URL https://example.com Timeout error.
+# [2024-03-02 14:32:45] INFO: Successfully categorized URL https://safe-site.com as SAFE.
+# [2024-03-02 14:35:20] WARNING: Suspicious activity detected on https://malware-site.com
+
+class Logger:
+    pass
