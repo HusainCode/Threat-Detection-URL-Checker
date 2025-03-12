e143a1c013cfdbf41fd8f81d781ab4cc8f7753e2
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-03-12 11:30:26 -0500

    Update api_handler.py

--- src/api_handler.py
+++ src/api_handler.py
@@ -14,8 +14,7 @@
 #
 #  Main Methods:
 #
-import queue
-import httpx
+
 import os  # Provides functions to interact with the operating system, including environment variables
 from dotenv import load_dotenv  # Loads environment variables from a .env file
 from pathlib import Path  # Handles file system paths in a cross-platform way
@@ -40,11 +39,6 @@ import asyncio
 
 import tracemalloc
 
-import json
-import csv
-import aiohttp
-import time
-
 tracemalloc.start()
 
 # Loads API key securely from .env file
@@ -65,10 +59,10 @@ class APIHandler:
 
         """
         source: https://cloud.google.com/web-risk/docs/reference/rest/v1/ThreatType
-        
+
         ThreatType:
         The type of threat. This maps directly to the threat list a threat may belong to.
-        
+
         1- THREAT_TYPE_UNSPECIFIED:
         	       No entries should match this threat type. This threat type is unused.
         2- MALWARE:	
@@ -79,7 +73,7 @@ class APIHandler:
                      Unwanted software targeting any platform.
         5- SOCIAL_ENGINEERING_EXTENDED_COVERAGE:	
                       A list of extended coverage social engineering URIs targeting any platform.
-        
+
         """
         valid_threats = frozenset(
             [
@@ -102,8 +96,7 @@ class APIHandler:
         # Create a connection to the Google API
         self.webrisk_client = WebRiskServiceClient(credentials=self.credentials)
 
-        self.semaphore = asyncio.Semaphore(APIHandler.CURRENT_LIMIT) # current limit is 5
-
+        self.semaphore = asyncio.Semaphore(APIHandler.CURRENT_LIMIT)  # current limit is 5
 
     # Source: https://cloud.google.com/web-risk/docs/lookup-api#python
     async def search_uri(self, uri: str) -> SearchUrisResponse:
@@ -130,8 +123,9 @@ class APIHandler:
         # Google Web Risk API only allows ONE URL per request
         loop = asyncio.events.get_running_loop()
 
-        request = SearchUrisRequest()  # Specify the type of threat
-        request.threat_types = self.THREAT_TYPE
+        # create a request object for the web risk google API
+        request = SearchUrisRequest()
+        request.threat_types = self.THREAT_TYPE  # Specify the type of threat
         request.uri = uri
 
         response = self.webrisk_client.search_uris(request)
@@ -156,17 +150,23 @@ class APIHandler:
             print(f"Internal server error: {e.message}")
 
     # Process the coming APIs list
-    async def worker(self, api):
-        await self.queue.put(api)
+    async def worker(self):
+        while not self.queue.empty():
+            await self.queue.put()
 
-        async with self.semaphore:
-            url = await self.queue.get()
-            await self.search_uri(url)
-            self.queue.task_done()  # Mark the task as done
+            async with self.semaphore:
+                url = await self.queue.get()
+                await self.search_uri(url)
+                self.queue.task_done()  # Mark the task as done
 
+    async def process_urls(self):
+        urls = self.CVSHandler.load_API_link()  # Load all URLs from CSV
 
-    # def logger_messanger(self):
-    #     pass
+        for url in urls:
+            await self.queue.put(url)  # add URLS to the queue
+
+        workers = [asyncio.create.task(self.worker()) for _ in range(APIHandler.CURRENT_LIMIT)]
+        await asyncio.gather(*workers)  # wait for workers to finsh
 
 
 # API = APIHandler()
@@ -178,4 +178,3 @@ class APIHandler:
 
 # snapshot = tracemalloc.take_snapshot()
 # top_stats = snapshot.statistics("lineno")
-
